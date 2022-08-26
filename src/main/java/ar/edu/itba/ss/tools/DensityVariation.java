dd4fff6abe28e4003dc2ea530fbbdf5ae2e2bb51
Julian Arce <juarce@itba.edu.ar>
2022-08-26 15:17:33 -0300

    feat: density variation script

---
+++ src/main/java/ar/edu/itba/ss/tools/DensityVariation.java
@@ -0,0 +1,18 @@
+package ar.edu.itba.ss.tools;
+
+import ar.edu.itba.ss.App;
+
+import java.util.List;
+
+public class DensityVariation {
+    public static void main(String[] args) {
+        final List<Integer> N = List.of(40, 100, 200, 300, 400, 500, 1000, 2000, 3000, 4000);
+
+        for(int n : N) {
+            String staticFilename = "src/main/resources/input/density/static_" + n + ".csv";
+            String dynamicFilename = "src/main/resources/input/density/dynamic_" + n + ".csv";
+            String outputFilename = "density/output_" + n + ".csv";
+            App.main(new String[]{staticFilename, dynamicFilename, outputFilename});
+        }
+    }
+}
