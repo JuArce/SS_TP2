1675e6e4709f47b707d22b96dba4b4a3ba19ad5c
Julian Arce <juarce@itba.edu.ar>
2022-08-28 18:58:38 -0300

    feat: noise variation simulations
    LFS*

--- src/main/java/ar/edu/itba/ss/NoiseVariation.java
+++ src/main/java/ar/edu/itba/ss/NoiseVariation.java
@@ -2,15 +2,20 @@ package ar.edu.itba.ss;
 
 import ar.edu.itba.ss.models.Particle;
 
+import java.util.List;
+
 public class NoiseVariation {
     public static void main(String[] args) {
         String particlePath = args[0];
         String positionPath = args[1];
 
-        for(int i = 1; i < 10; i++) {
-            String outputFilename = "noise/output_n_" + i + ".csv";
-            Particle.n = i;
+        final List<Double> noise = List.of(0.0, 0.1, 0.5, 1.0, 1.5, 2.0);
+
+        noise.forEach(n ->{
+            String outputFilename = "noise/output_n_" + n + ".csv";
+            Particle.n = n;
             App.main(new String[]{particlePath, positionPath, outputFilename});
-        }
+            Particle.sequence = 1;
+        });
     }
 }
--- src/main/java/ar/edu/itba/ss/tools/Random.java
+++ src/main/java/ar/edu/itba/ss/tools/Random.java
@@ -4,6 +4,9 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Random {
     public static double getRandom(double min, double max) {
+        if (min >= max) {
+            return 0;
+        }
         return ThreadLocalRandom.current().nextDouble(min, max);
     }
 }
