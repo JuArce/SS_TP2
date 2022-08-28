6293a81a72296a078cad7e2d276ad105a2d0deaa
Gian <gpecile@itba.edu.ar>
2022-08-28 20:36:52 -0300

    fix: name

--- src/main/java/ar/edu/itba/ss/DensityVariation.java
+++ src/main/java/ar/edu/itba/ss/DensityVariation.java
@@ -11,7 +11,7 @@ public class DensityVariation {
         for(int n : N) {
             String staticFilename = "src/main/resources/input/density/static_" + n + ".csv";
             String dynamicFilename = "src/main/resources/input/density/dynamic_" + n + ".csv";
-            String outputFilename = "density/output_" + n + ".csv";
+            String outputFilename = "density/output_N_" + n + ".csv";
             App.main(new String[]{staticFilename, dynamicFilename, outputFilename});
             Particle.sequence = 1;
         }
