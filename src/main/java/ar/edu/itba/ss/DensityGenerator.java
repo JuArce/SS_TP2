8634240e5cbd3bbb77acee5aaa7b75843b4dd3e7
Julian Arce <juarce@itba.edu.ar>
2022-08-29 11:18:20 -0300

    fix: set Locale.root

--- src/main/java/ar/edu/itba/ss/DensityGenerator.java
+++ src/main/java/ar/edu/itba/ss/DensityGenerator.java
@@ -3,6 +3,7 @@ package ar.edu.itba.ss;
 import ar.edu.itba.ss.models.Particle;
 
 import java.util.List;
+import java.util.Locale;
 
 public class DensityGenerator {
     public static void main(String[] args) {
@@ -12,8 +13,8 @@ public class DensityGenerator {
         final String extension = ".csv";
 
         N.forEach(particles -> noise.forEach(n -> {
-            String staticFilename = String.format("density/static_N_%d_L_%d_n_%f_%s", particles, L, n, extension);
-            String dynamicFilename = String.format("density/dynamic_N_%d_L_%d_n_%f_%s", particles, L, n, extension);
+            String staticFilename = String.format(Locale.ROOT ,"density/static_N_%d_L_%d_n_%.1f%s", particles, L, n, extension);
+            String dynamicFilename = String.format(Locale.ROOT, "density/dynamic_N_%d_L_%d_n_%.1f%s", particles, L, n, extension);
             Particle.n = n;
             Generator.main(new String[]{String.valueOf(particles), String.valueOf(L), staticFilename, dynamicFilename});
             Particle.sequence = 1;
--- src/main/java/ar/edu/itba/ss/DensityVariation.java
+++ src/main/java/ar/edu/itba/ss/DensityVariation.java
@@ -3,24 +3,19 @@ package ar.edu.itba.ss;
 import ar.edu.itba.ss.models.Particle;
 
 import java.util.List;
+import java.util.Locale;
 
 public class DensityVariation {
     public static void main(String[] args) {
         final List<Integer> N = List.of(10, 25, 50, 100, 250, 500, 1000, 1500);
         final List<Double> noise = List.of(0.0, 1.0, 5.0);
         final int L = 10;
+        final String extension = ".csv";
 
-        for(int n : N) {
-            String staticFilename = "src/main/resources/input/density/static_" + n + ".csv";
-            String dynamicFilename = "src/main/resources/input/density/dynamic_" + n + ".csv";
-            String outputFilename = "density/output_N_" + n + ".csv";
-            App.main(new String[]{staticFilename, dynamicFilename, outputFilename});
-            Particle.sequence = 1;
-        }
         N.forEach(particles -> noise.forEach(n -> {
-            String staticFilename = String.format("src/main/resources/input/density/static_N_%d_L_%d_n_%f.csv", particles, L, n);
-            String dynamicFilename = String.format("src/main/resources/input/density/dynamic_N_%d_L_%d_n_%f.csv", particles, L, n);
-            String outputFilename = String.format("density/output_N_%d_L_%d_n_%f.csv", particles, L, n);
+            String staticFilename = String.format(Locale.ROOT ,"src/main/resources/input/density/static_N_%d_L_%d_n_%f%s", particles, L, n, extension);
+            String dynamicFilename = String.format(Locale.ROOT ,"src/main/resources/input/density/dynamic_N_%d_L_%d_n_%f%s", particles, L, n, extension);
+            String outputFilename = String.format(Locale.ROOT ,"density/output_N_%d_L_%d_n_%.1f.csv", particles, L, n);
             Particle.n = n;
             App.main(new String[]{staticFilename, dynamicFilename, outputFilename});
             Particle.sequence = 1;
