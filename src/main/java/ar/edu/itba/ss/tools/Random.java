f9410c7e4749b50bd7b7a21e1357c24944ef305c
Julian Arce <juarce@itba.edu.ar>
2022-08-30 09:22:17 -0300

    fix: random generator validations

--- src/main/java/ar/edu/itba/ss/tools/Random.java
+++ src/main/java/ar/edu/itba/ss/tools/Random.java
@@ -3,9 +3,11 @@ package ar.edu.itba.ss.tools;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Random {
+    public static final double epsilon = 0.000001;
+
     public static double getRandom(double min, double max) {
-        if (min >= max) {
-            return 0;
+        if (Math.abs(max - min) < epsilon || min > max) {
+            return 0.0;
         }
         return ThreadLocalRandom.current().nextDouble(min, max);
     }
